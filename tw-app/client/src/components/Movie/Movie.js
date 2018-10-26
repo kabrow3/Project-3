2a7d6c5a4e6b93f67dd02960b825fb91929654a9
kabrow3 <kabrow3@gmail.com>
2018-10-26 14:44:02 -0400

    minor formatting

--- tw-app/client/src/components/Movie/Movie.js
+++ tw-app/client/src/components/Movie/Movie.js
@@ -4,34 +4,27 @@ import "./Movie.css";
 
 const Movie = props => (
   <div className='row'>
-    <div className="col-md-6">
-      <div className="movie-poster">
-        <img src={props.poster}></img>
+    <div className="movie-content">
+      <div className="row">
+        <h1 className="title">{props.title}<span id="year">{props.year}</span></h1>
       </div>
+    <div className="row">
+        <span id="rated">{props.rated}</span> | 
+        <span id="runtime">{props.runtime}</span> | 
+        <span id="genres">{props.genre}</span>
     </div>
-    <div className="col-md-6">
-      <div className="movie-content">
-        <div className="row">
-          <h1 className="title">{props.title}<span id="year">{props.year}</span></h1>
-        </div>
-      <div className="row">
-          <span id="rated">{props.rated}</span> | 
-          <span id="runtime">{props.runtime}</span> | 
-          <span id="genres">{props.genre}</span>
-      </div>
-      <br />
-      <div className="row">
-          <h4>Director <span id="director">{props.director}</span></h4>
-          <br />
-          <h4>Writer <span id="writer">{props.writer}</span></h4>
-          <br />
-          <h4>Cast <span id="actors">{props.actors}</span></h4>
-      </div>
-      <br />
-      <div className="row">
-          <p id="plot">{props.plot}</p>
-      </div>
-      </div>
+    <br />
+    <div className="row">
+        <h4>Director <span id="director">{props.director}</span></h4>
+        <br />
+        <h4>Writer <span id="writer">{props.writer}</span></h4>
+        <br />
+        <h4>Cast <span id="actors">{props.actors}</span></h4>
+    </div>
+    <br />
+    <div className="row">
+        <p id="plot">{props.plot}</p>
+    </div>
     </div>
   </div>
   );
