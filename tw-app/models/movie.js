2f5fdca890696399ed23f5e315fbc9f1b1e23634
rherd <rob.a.herd@gmail.com>
2018-10-27 12:31:31 -0400

    primary key on imdbid

--- tw-app/models/movie.js
+++ tw-app/models/movie.js
@@ -1,7 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
     var Movie = sequelize.define("Movie", {
       title: DataTypes.STRING,
-      imdbID: DataTypes.STRING
+      imdbID: {
+        type: DataTypes.STRING,
+        primaryKey: true // Automatically gets converted to SERIAL for postgres
+      }
     });
   
     Movie.associate = function(models) {
